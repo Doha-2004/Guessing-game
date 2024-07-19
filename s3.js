c1462845f4e6af33c8e135aedcc6c748f70cf824
Doha Emad <125505489+Doha-2004@users.noreply.github.com>
2024-07-19 22:41:22 +0300

    Update s3.js

--- s3.js
+++ s3.js
@@ -1,13 +1,14 @@
 let randomNumber = Math.floor(Math.random() * 100 + 1);
 let min = 1;
 let max = 100;
+let attemps=0;
 do{
 function check() {
   const usernumber = parseInt( document.getElementById('guessInput').value);
   const result = document.getElementById('result');
-  
+   attemps++;
   if (usernumber == randomNumber) {
-      result.textContent = `Congrats! You guessed the correct number ${randomNumber} `;
+      result.textContent = `Congrats! You guessed the correct number ${randomNumber} ,You found the number on try number ${attemps}`;
   } else if (usernumber > randomNumber) {
       result.textContent = 'The number you guessed is greater than the correct number. Try again';
       max = usernumber - 1;
